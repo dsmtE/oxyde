bb86a146ad5166a0a9d1c723949ef65043bd0216
dsmtE <desmet.enguerrand@gmail.com>
2022-07-20 13:53:03 +0200

    ♻️  use new binding builder

--- src/wgpu_utils/ping_pong_texture.rs
+++ src/wgpu_utils/ping_pong_texture.rs
@@ -1,10 +1,12 @@
+use super::binding_builder::{ BindGroupLayoutBuilder, BindGroupBuilder, BindGroupLayoutWithDesc};
+
 pub struct PingPongTexture {
     label: Option<&'static str>,
     // texture_ping: wgpu::Texture,
     // texture_pong: wgpu::Texture,
     view_ping: wgpu::TextureView,
     view_pong: wgpu::TextureView,
-    pub bind_group_layout: wgpu::BindGroupLayout,
+    pub bind_group_layout: BindGroupLayoutWithDesc,
     pub state: bool,
 }
 
@@ -20,28 +22,17 @@ impl PingPongTexture {
         let view_ping = texture_ping.create_view(&wgpu::TextureViewDescriptor::default());
         let view_pong = texture_pong.create_view(&wgpu::TextureViewDescriptor::default());
 
-        let group_layout_label: String = label.unwrap_or("").to_owned() + " bind group layout";
-        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-            entries: &[
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Texture {
-                        multisampled: false,
-                        view_dimension: wgpu::TextureViewDimension::D2,
-                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
-                    },
-                    count: None,
-                },
-                wgpu::BindGroupLayoutEntry {
-                    binding: 1,
-                    visibility: wgpu::ShaderStages::FRAGMENT,
-                    ty: wgpu::BindingType::Sampler { 0: wgpu::SamplerBindingType::Filtering },
-                    count: None,
-                },
-            ],
-            label: Some(&group_layout_label[..]),
-        });
+        let bind_group_layout = BindGroupLayoutBuilder::new()
+            .add_binding_fragment(
+                wgpu::BindingType::Texture {
+                    multisampled: false,
+                    view_dimension: wgpu::TextureViewDimension::D2,
+                    sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                }
+            ).add_binding_fragment(
+                wgpu::BindingType::Sampler { 0: wgpu::SamplerBindingType::Filtering }
+            )
+            .create(device, format!("BindGroupLayout: {}", label.unwrap_or("")).as_str());
 
         Ok(Self {
             label,
@@ -53,37 +44,16 @@ impl PingPongTexture {
     }
 
     pub fn create_binding_group(&self, device: &wgpu::Device, sampler: &wgpu::Sampler) -> (wgpu::BindGroup, wgpu::BindGroup) {
-        let bind_group_ping_label: String = self.label.unwrap_or("").to_owned() + " bind group ping";
-        let bind_group_ping = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &self.bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&self.view_ping),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(sampler),
-                },
-            ],
-            label: Some(&bind_group_ping_label[..]),
-        });
 
-        let bind_group_pong_label: String = self.label.unwrap_or("").to_owned() + " bind group pong";
-        let bind_group_pong = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &self.bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&self.view_pong),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(sampler),
-                },
-            ],
-            label: Some(&bind_group_pong_label[..]),
-        });
+        let bind_group_ping = BindGroupBuilder::new(&self.bind_group_layout)
+            .texture(&self.view_ping)
+            .sampler(sampler)
+            .create(device, format!("BindGroup(ping): {}", self.label.unwrap_or("")).as_str());
+
+        let bind_group_pong = BindGroupBuilder::new(&self.bind_group_layout)
+        .texture(&self.view_pong)
+        .sampler(sampler)
+        .create(device, format!("BindGroup(pong): {}", self.label.unwrap_or("")).as_str());
 
         (bind_group_ping, bind_group_pong)
     }
