2122ab25572cb582c0bee1b768905617b5ec4c27
dsmtE <desmet.enguerrand@gmail.com>
2022-08-13 11:51:50 +0200

    ✨ add shaderCompilation from code as str

--- src/wgpu_utils/shaders.rs
+++ src/wgpu_utils/shaders.rs
@@ -7,23 +7,30 @@ use std::{
 
 use anyhow::{Context, Result};
 
+pub enum Source {
+    File(PathBuf),
+    Code(String),
+}
+
+pub use shaderc::ShaderKind;
+
 pub struct ShaderModuleWithSourceFiles {
     pub module: wgpu::ShaderModule,
     // main source file and all includes
-    pub source_files: Vec<PathBuf>,
+    pub source_files: Vec<Source>,
 }
 
 // compile glsl shadermodule using spirv
 // TODO: try wgpuglsl feature instead
 pub fn load_glsl_shader_module_from_path(device: &wgpu::Device, path: &Path, entry_point_name: &'static str) -> Result<ShaderModuleWithSourceFiles> {
-    let source_files = RefCell::new(vec![path.canonicalize().unwrap()]);
+    let source_files = RefCell::new(vec![Source::File(path.canonicalize().unwrap())]);
 
     let glsl_code = std::fs::read_to_string(&path).with_context(|| format!("Failed to read shader file \"{:?}\"", path))?;
 
     let kind = match path.extension().and_then(OsStr::to_str) {
-        Some("frag") => shaderc::ShaderKind::Fragment,
-        Some("vert") => shaderc::ShaderKind::Vertex,
-        Some("comp") => shaderc::ShaderKind::Compute,
+        Some("frag") => ShaderKind::Fragment,
+        Some("vert") => ShaderKind::Vertex,
+        Some("comp") => ShaderKind::Compute,
         _ => {
             return Err(anyhow::anyhow!("Did not recognize file extension for shader file \"{:?}\"", path));
         },
@@ -38,22 +45,10 @@ pub fn load_glsl_shader_module_from_path(device: &wgpu::Device, path: &Path, ent
         options.set_optimization_level(shaderc::OptimizationLevel::Performance);
         options.set_generate_debug_info();
 
-        options.add_macro_definition(
-            "FRAGMENT_SHADER",
-            Some(if kind == shaderc::ShaderKind::Fragment {
-                "1"
-            } else {
-                "0"
-            }),
-        );
-        options.add_macro_definition("VERTEX_SHADER", Some(if kind == shaderc::ShaderKind::Vertex { "1" } else { "0" }));
-        options.add_macro_definition("COMPUTE_SHADER", Some(if kind == shaderc::ShaderKind::Compute { "1" } else { "0" }));
-
-        if cfg!(debug_assertions) {
-            options.add_macro_definition("DEBUG", Some("1"));
-        } else {
-            options.add_macro_definition("NDEBUG", Some("1"));
-        }
+        options.add_macro_definition("FRAGMENT_SHADER", Some(if kind == ShaderKind::Fragment { "1" } else { "0" }));
+        options.add_macro_definition("VERTEX_SHADER", Some(if kind == ShaderKind::Vertex { "1" } else { "0" }));
+        options.add_macro_definition("COMPUTE_SHADER", Some(if kind == ShaderKind::Compute { "1" } else { "0" }));
+        options.add_macro_definition(if cfg!(debug_assertions) {"DEBUG"} else {"NDEBUG"} , Some("1"));
 
         options.set_include_callback(|name, include_type, source_file, _depth| {
             let path = if include_type == shaderc::IncludeType::Relative {
@@ -66,7 +61,7 @@ pub fn load_glsl_shader_module_from_path(device: &wgpu::Device, path: &Path, ent
             };
             match std::fs::read_to_string(&path) {
                 Ok(glsl_code) => {
-                    source_files.borrow_mut().push(path.canonicalize().unwrap());
+                    source_files.borrow_mut().push(Source::File(path.canonicalize().unwrap()));
                     Ok(shaderc::ResolvedInclude {
                         resolved_name: String::from(name),
                         content: glsl_code,
@@ -91,6 +86,90 @@ pub fn load_glsl_shader_module_from_path(device: &wgpu::Device, path: &Path, ent
     Ok(ShaderModuleWithSourceFiles {
         module: device.create_shader_module(&wgpu::ShaderModuleDescriptor {
             label: Some(path.file_name().unwrap().to_str().unwrap()),
+            source: wgpu::ShaderSource::SpirV(Borrowed(&compilation_artifact.as_binary())),
+        }),
+        source_files: source_files.into_inner(),
+    })
+}
+
+pub fn load_glsl_shader_module_from_string(
+    device: &wgpu::Device,
+    glsl_code: &String,
+    kind: ShaderKind,
+    entry_point_name: &'static str,
+    include_paths: Vec<&'static str>,
+    label: Option<&str>,
+
+) -> Result<ShaderModuleWithSourceFiles> {
+
+    let source_files = RefCell::new(vec![Source::Code(glsl_code.to_owned())]);
+
+    let compilation_artifact = {
+        let compiler = shaderc::Compiler::new().unwrap();
+        let mut options = shaderc::CompileOptions::new().unwrap();
+
+        options.set_warnings_as_errors();
+        options.set_target_env(shaderc::TargetEnv::Vulkan, 0);
+        options.set_optimization_level(shaderc::OptimizationLevel::Performance);
+        options.set_generate_debug_info();
+
+        options.add_macro_definition("FRAGMENT_SHADER", Some(if kind == ShaderKind::Fragment { "1" } else { "0" }));
+        options.add_macro_definition("VERTEX_SHADER", Some(if kind == ShaderKind::Vertex { "1" } else { "0" }));
+        options.add_macro_definition("COMPUTE_SHADER", Some(if kind == ShaderKind::Compute { "1" } else { "0" }));
+        options.add_macro_definition(if cfg!(debug_assertions) {"DEBUG"} else {"NDEBUG"} , Some("1"));
+
+        options.set_include_callback(|name, include_type, source_file, _depth| {
+            if include_type == shaderc::IncludeType::Standard {
+                return Err(format!(
+                    "Unable to handle standard IncludeType for shader file\"{:?}\" for include \"{:?}\"",
+                    source_file, name
+                ));
+            };
+            
+            let possible_paths = include_paths.iter()
+                .map(|path| Path::new(&path).join(name))
+                .filter(|path| path.exists()).collect::<Vec::<PathBuf>>();
+            
+            if possible_paths.len() == 0 {
+                return Err(format!("Unable to find the file \"{}\" in listed include_paths",
+                            name
+                        ));
+            }else if  possible_paths.len() > 1 {
+                return Err(format!(
+                    "Multiples files found for the same include name \"{}\" in listed include_paths",
+                    name
+                ));
+            }
+
+            let path = possible_paths.first().unwrap();
+            match std::fs::read_to_string(path) {
+                Ok(glsl_code) => {
+                    debug!("Include to {} in {} resolved at path: {:?}", name, source_file, path);
+                    source_files.borrow_mut().push(Source::File(path.canonicalize().unwrap()));
+                    Ok(shaderc::ResolvedInclude {
+                        resolved_name: String::from(name),
+                        content: glsl_code,
+                    })
+                },
+                Err(err) => Err(format!(
+                    "Failed to resolve include to {} in {} (was looking for {:?}): {}",
+                    name, source_file, path, err
+                )),
+            }
+        });
+
+        compiler
+            .compile_into_spirv(&glsl_code, kind, label.unwrap_or("unknown"), entry_point_name, Some(&options))
+            .with_context(|| "Failed to compile shader from string")?
+    };
+    
+    if compilation_artifact.get_num_warnings() > 0 {
+        warn!("warnings when compiling:\n{}", compilation_artifact.get_warning_messages());
+    }
+
+    Ok(ShaderModuleWithSourceFiles {
+        module: device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label,
             source: wgpu::ShaderSource::SpirV(Borrowed(&compilation_artifact.as_binary())),
         }),
         source_files: source_files.into_inner(),
