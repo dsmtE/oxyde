c93b8d6148555f7053e1713d038228329dded328
dsmtE <desmet.enguerrand@gmail.com>
2023-03-15 20:26:59 +0100

    🔨 make shaderc optional dependency

--- src/wgpu_utils/mod.rs
+++ src/wgpu_utils/mod.rs
@@ -2,7 +2,10 @@ pub mod binding_builder;
 pub mod binding_glsl;
 mod ping_pong_buffer;
 mod ping_pong_texture;
+
+#[cfg(feature = "glsl")]
 pub mod shaders;
+
 pub mod uniform_buffer;
 
 pub use ping_pong_buffer::PingPongBuffer;
