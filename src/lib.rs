c4e1609f1eed8b80a5a51fc3a04076960ad91bfb
dsmtE <desmet.enguerrand@gmail.com>
2024-02-22 01:06:28 +0100

    🧑‍💻 Expose internal crates

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,9 @@ pub use input::InputsState;
 pub extern crate egui;
 pub extern crate wgpu;
 pub extern crate winit;
+pub extern crate bytemuck;
+
+pub extern crate anyhow;
 
 #[macro_use]
 extern crate log;
