ddede70e881451db79ad549fd61ceab5abff8d89
dsmtE <desmet.enguerrand@gmail.com>
2024-02-22 18:18:32 +0100

    🗑️ remove deprecated SingleBufferWrapper

--- src/wgpu_utils/buffers.rs
+++ src/wgpu_utils/buffers.rs
@@ -1,108 +1,5 @@
-use super::binding_builder;
-
 use wgpu::{Buffer, BufferAddress, BufferDescriptor, BufferUsages, CommandEncoder, Device, Queue};
 
-use std::mem::size_of;
-
-#[deprecated(note = "better to use binding_builder helpers to create bind_group manually")]
-pub struct SingleBufferWrapper {
-    buffer: wgpu::Buffer,
-    bind_group_layout_with_desc: binding_builder::BindGroupLayoutWithDesc,
-    bind_group: wgpu::BindGroup,
-}
-
-impl SingleBufferWrapper {
-    pub fn new(
-        device: &wgpu::Device,
-        size: u64,
-        usages: wgpu::BufferUsages,
-        visibility: wgpu::ShaderStages,
-        ty: wgpu::BufferBindingType,
-        has_dynamic_offset: bool,
-        label: Option<&str>,
-    ) -> Self {
-        let buffer = device.create_buffer(&wgpu::BufferDescriptor {
-            label: Some(format!("{} Buffer", label.unwrap_or("unknown")).as_str()),
-            size,
-            usage: usages,
-            mapped_at_creation: false,
-        });
-
-        let (bind_group_layout_with_desc, bind_group) =
-            Self::create_layout_and_bind_group(device, &buffer, visibility, ty, has_dynamic_offset, wgpu::BufferSize::new(size), label);
-
-        Self {
-            buffer,
-            bind_group_layout_with_desc,
-            bind_group,
-        }
-    }
-
-    pub fn new_from_data<T>(
-        device: &wgpu::Device,
-        slice_content: &[T],
-        usages: wgpu::BufferUsages,
-        visibility: wgpu::ShaderStages,
-        ty: wgpu::BufferBindingType,
-        has_dynamic_offset: bool,
-        label: Option<&str>,
-    ) -> Self
-    where
-        T: bytemuck::Pod,
-    {
-        let buffer = wgpu::util::DeviceExt::create_buffer_init(
-            device,
-            &wgpu::util::BufferInitDescriptor {
-                label: Some(format!("{} Buffer", label.unwrap_or("unknown")).as_str()),
-                contents: bytemuck::cast_slice(slice_content),
-                usage: usages,
-            },
-        );
-
-        let min_binding_size = wgpu::BufferSize::new((slice_content.len() * std::mem::size_of::<T>()) as u64);
-        let (bind_group_layout_with_desc, bind_group) =
-            Self::create_layout_and_bind_group(device, &buffer, visibility, ty, has_dynamic_offset, min_binding_size, label);
-
-        Self {
-            buffer,
-            bind_group_layout_with_desc,
-            bind_group,
-        }
-    }
-
-    pub fn bind_group(&self) -> &wgpu::BindGroup { &self.bind_group }
-
-    pub fn buffer(&self) -> &wgpu::Buffer { &self.buffer }
-
-    pub fn layout(&self) -> &wgpu::BindGroupLayout { &self.bind_group_layout_with_desc.layout }
-
-    fn create_layout_and_bind_group(
-        device: &wgpu::Device,
-        buffer: &wgpu::Buffer,
-        visibility: wgpu::ShaderStages,
-        ty: wgpu::BufferBindingType,
-        has_dynamic_offset: bool,
-        min_binding_size: Option<wgpu::BufferSize>,
-        label: Option<&str>,
-    ) -> (binding_builder::BindGroupLayoutWithDesc, wgpu::BindGroup) {
-        let label = label.unwrap_or("unknown");
-
-        let bind_group_layout_with_desc = binding_builder::BindGroupLayoutBuilder::new()
-            .add_binding(visibility, wgpu::BindingType::Buffer { ty, has_dynamic_offset, min_binding_size })
-            .create(device, Some(format!("{} BindGroupLayout", label).as_str()));
-
-        let bind_group = binding_builder::BindGroupBuilder::new(&bind_group_layout_with_desc)
-            .resource(buffer.as_entire_binding())
-            .create(device, Some(format!("{} BindGroup", label).as_str()));
-
-        (bind_group_layout_with_desc, bind_group)
-    }
-}
-
-pub enum StagingBufferAccess {
-    Read,
-    Write,
-}
 // Buffer wrapper for a GPU buffer that can be read or write from the CPU (using intermediate staging buffer)
 pub struct StagingBufferWrapper<T: bytemuck::Pod, const READ_OR_WRITE: bool> {
     values: Vec<T>,
@@ -190,7 +87,7 @@ impl<T: bytemuck::Pod, const READ_OR_WRITE: bool> StagingBufferWrapper<T, READ_O
     #[inline]
     pub fn is_empty(&self) -> bool { self.values.is_empty() }
     #[inline]
-    pub fn bytes_size(&self) -> usize { self.len() * size_of::<T>() }
+    pub fn bytes_size(&self) -> usize { self.len() * std::mem::size_of::<T>() }
     #[inline]
     pub fn values_as_slice(&self) -> &[T] { self.values.as_slice() }
     #[inline]
